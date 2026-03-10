5407152a4ba72f9ecad348100f541f42137ef923
BENSIX6 <ben_lunique66@hotmail.fr>
2026-03-10 13:59:32 +0000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+// Clé API Anthropic injectée depuis les variables d'environnement Vite
+// Sur Netlify : ajouter VITE_ANTHROPIC_KEY dans les env vars du projet
+window.ANTHROPIC_KEY = import.meta.env.VITE_ANTHROPIC_KEY || ""
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
